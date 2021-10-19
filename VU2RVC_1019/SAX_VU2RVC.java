20a51067e0244199e704fe33a2144bb94155fc88
Csurgai Ádám <adam.csurgai@gmail.com>
2021-10-19 15:19:12 +0200

    initial commit

---
+++ VU2RVC_1019/SAX_VU2RVC.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+
+import java.xml.parsers.ParsersConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SaxMPJYO8 extends DefaultHandler {
+
+    private int indent = 0;
+
+    private String formatAttributes(Attributes attributes) {
+        int attrlength = attributes.getLength();
+        
+    }
+}
